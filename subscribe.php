1dfe0e725fb6219227013e60f70adad94604720b
Moses Grey <61410416+grey-lab@users.noreply.github.com>
2023-05-31 11:04:57 +0300

    Responsive Contact form

--- subscribe.php
+++ subscribe.php
@@ -1,9 +1,14 @@
-<? php 
-if($_POST['message']) {
-	mail("mosesgrey5@gmail.com",
-	 subject, 
-	 $_POST['message']
-	)
-}
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["message"])) {
+    $to = "mosesgrey20@gmail.com";
+    $subject = "Contact Form";
+    $message = $_POST["message"];
+    $headers = "From: " . $_POST["email"] . "\r\n";
 
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Email sending failed.";
+    }
+}
 ?>
